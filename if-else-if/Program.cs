ee18337dddd57797ab29c4829714b0af32d7751f
Muhammed Ipekliogullari <muhammednecati20@gmail.com>
2023-02-28 16:32:33 +0300

    Add project files.

---
+++ if-else-if/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+
+        if (time >= 6 && time < 11 )
+            Console.WriteLine("Günaydın");
+
+        else if (time <= 16)
+            Console.WriteLine("İyi Günler");
+
+        else
+            Console.WriteLine("İyi Geceler");
+
+        string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+
+        sonuc = time >= 6 && time < 17 ? "Günaydın" : time <= 18 ? "iyi günler" : "iyi geceler";
+        Console.WriteLine(sonuc);
+
+
+
+    }
+}
